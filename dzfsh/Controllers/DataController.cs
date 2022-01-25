fb3a75bb514fc51bb65a726fb6a5c7ec53976942
Sebastian <rodriguezse13@gmail.com>
2022-01-24 19:27:36 -0500

    Project

---
+++ dzfsh/Controllers/DataController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using dzfsh;
+
+namespace dzfsh.Controllers
+{
+    public class DataController : Controller
+    {
+        private EquiposEntities1 db = new EquiposEntities1();
+
+        // GET: Data
+        public ActionResult Index()
+        {
+            var res = new List<Data>();
+            var multa = db.Data.ToList();
+            //(EndDate - StartDate).Days
+            foreach(var v in multa)
+            {
+                if(v.FechaEntr > v.FechaFin)
+                {
+                    var today = DateTime.Now;
+                    var mul = (v.FechaEntr - (v.FechaFin != null ? v.FechaFin : today)).Value.Days;
+                    v.Multa = (mul * 5);
+                }
+                else 
+                {
+                    v.Multa = 0;
+                }
+            }
+
+            return View(db.Data.ToList());
+        }
+
+        // GET: Data/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Data data = db.Data.Find(id);
+            if (data == null)
+            {
+                return HttpNotFound();
+            }
+            return View(data);
+        }
+
+        // GET: Data/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Data/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,NombreEquipo,FechaIni,FechaFin,FechaEntr,Usuario,Estado,Equipo,Multa")] Data data)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Data.Add(data);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(data);
+        }
+
+        // GET: Data/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Data data = db.Data.Find(id);
+            if (data == null)
+            {
+                return HttpNotFound();
+            }
+            return View(data);
+        }
+
+        // POST: Data/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,NombreEquipo,FechaIni,FechaFin,FechaEntr,Usuario,Estado,Equipo,Multa")] Data data)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(data).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+
+        // GET: Data/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Data data = db.Data.Find(id);
+            if (data == null)
+            {
+                return HttpNotFound();
+            }
+            return View(data);
+        }
+
+        // POST: Data/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Data data = db.Data.Find(id);
+            db.Data.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
